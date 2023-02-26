17ca5ec75ce5c34d49fded7f02fc8965c1b72002
Marcin Bartmiński <mbdjj@o2.pl>
2023-02-26 22:26:19 +0100

    Initial Commit

---
+++ Raycaster/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Raycaster
+//
+//  Created by Marcin Bartminski on 26/02/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
